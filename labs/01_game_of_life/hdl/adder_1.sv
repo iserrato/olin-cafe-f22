27981f950a245d877907c0721d457c910ae24c26
Isabel Serrato <iserrato@olin.edu>
2022-09-29 11:19:28 -0400

    Implement decoder and adder with tests

--- labs/01_game_of_life/hdl/adder_1.sv
+++ labs/01_game_of_life/hdl/adder_1.sv
@@ -10,5 +10,13 @@ module adder_1(a, b, c_in, sum, c_out);
 input wire a, b, c_in;
 output logic sum, c_out;
 
+logic a_bar, b_bar, c_in_bar;
+always_comb begin
+  a_bar = ~ a;
+  b_bar = ~ b;
+  c_in_bar = ~ c_in;
+  sum = (a ^ b) ^ c_in;
+  c_out = ((a ^ b) & c_in) + (a & b);
+end
 
 endmodule
---
+++ labs/01_game_of_life/tests/test_decoder_1_2.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+`define SIMULATION
+
+module test_decoders;
+  logic ena;
+  logic in;
+  wire [1:0] out;
+
+  decoder_1_to_2 UUT(
+    // .name of module_port(name_of_local_wire_or_logic)
+    // to avoid confusion about which port is which
+    .ena(ena), 
+    .in(in), 
+    .out(out)
+    );
+
+  initial begin
+    // Collect waveforms
+    $dumpfile("decoder_1_2.fst");
+    $dumpvars(0, UUT);
+    
+    ena = 1;
+    $display("ena in | out");
+    for (int i = 0; i < 2; i = i + 1) begin
+      in = i[0];
+      #1 $display("%1b %2b | %4b", ena, in, out);
+    end
+
+    ena = 0;
+    for (int i = 0; i < 2; i = i + 1) begin
+      in = i[0];
+      #1 $display("%1b %2b | %4b", ena, in, out);
+    end
+        
+    $finish;      
+	end
+
+endmodule
