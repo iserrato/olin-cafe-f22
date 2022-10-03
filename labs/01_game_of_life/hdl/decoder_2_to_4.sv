8bddcb8e83c5eede8be9ea78957e0cac2fd5eb1a
Isabel Serrato <iserrato@olin.edu>
2022-10-03 19:54:03 -0400

    complete 2 to 4 decoder

--- labs/01_game_of_life/hdl/decoder_2_to_4.sv
+++ labs/01_game_of_life/hdl/decoder_2_to_4.sv
@@ -5,5 +5,20 @@ input wire ena;
 input wire [1:0] in;
 output logic [3:0] out;
 
+logic ena_0;
+assign ena_0 = ~in[1] & ena;
+logic ena_1;
+assign ena_1 = in[1] & ena;
+decoder_1_to_2 DECODER1 (
+    .ena(ena_0),
+    .in(in[0]),
+    .out(out[1:0])
+);
+
+decoder_1_to_2 DECODER2 (
+    .ena(ena_1),
+    .in(in[0]),
+    .out(out[3:2])   
+);
 
 endmodule
