7a9e0a3ae5888c60c4d1c92a004f8592458fdc45
Isabel Serrato <iserrato@olin.edu>
2022-10-12 14:33:29 -0400

    complete 3 to 8 decoder

--- labs/01_game_of_life/hdl/decoder_3_to_8.sv
+++ labs/01_game_of_life/hdl/decoder_3_to_8.sv
@@ -6,4 +6,29 @@ module decoder_3_to_8(ena, in, out);
   output logic [7:0] out;
 
 
+  logic [1:0] ena_out;
+  decoder_1_to_2 ENA_DECODER (
+    .ena(ena),
+    .in(in[2]),
+    .out(ena_out[1:0])
+  );
+
+  logic ena_0; 
+  assign ena_0 = ena_out[0];
+  logic ena_1;
+  assign ena_1 = ena_out[1];
+
+  decoder_2_to_4 DECODER0 (
+    .ena(ena_0),
+    .in(in[1:0]),
+    .out(out[3:0])
+  );
+
+  decoder_2_to_4 DECODER1 (
+      .ena(ena_1),
+      .in(in[1:0]),
+      .out(out[7:4])
+  );
+
+
 endmodule
