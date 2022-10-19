505ac9586508ef43cc8b8b1a027183fd762a1f9d
Isabel Serrato <iserrato@olin.edu>
2022-10-18 21:39:42 -0400

    try running program

--- labs/01_game_of_life/build.tcl
+++ labs/01_game_of_life/build.tcl
@@ -9,8 +9,8 @@ read_xdc ./main.xdc
 # Sythesis & Optimization
 # Parts are either xc7a35tcpg236-1 or xc7a15tcpg236-1
 # Select the right one based on the sticker on your board!
-# synth_design -top main -part xc7a35tcpg236-1
-synth_design -top main -part xc7a15tcpg236-1
+synth_design -top main -part xc7a35tcpg236-1
+# synth_design -top main -part xc7a15tcpg236-1
 write_checkpoint -force synthesis.checkpoint
 opt_design
 # power_opt_design # optional till later.
