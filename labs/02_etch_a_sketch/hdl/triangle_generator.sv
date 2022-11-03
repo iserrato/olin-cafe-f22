754c36c4d8cd8abfa7fd452e29c5e6c117d3aff6
Isabel Serrato <iserrato@olin.edu>
2022-11-03 10:23:03 -0400

    complete part 1

--- labs/02_etch_a_sketch/hdl/triangle_generator.sv
+++ labs/02_etch_a_sketch/hdl/triangle_generator.sv
@@ -9,5 +9,33 @@ output logic [N-1:0] out;
 typedef enum logic {COUNTING_UP, COUNTING_DOWN} state_t;
 state_t state;
 
+always_ff @(posedge clk) begin
+  if (rst) begin
+    out <= 0;
+    state <= COUNTING_UP;
+  end
+  else begin 
+    if (ena) begin
+      case (state)
+        COUNTING_DOWN: begin
+          if (out == 0) begin
+            state <= COUNTING_UP;
+            out <= out + 1;
+          end
+          else out <= out - 1;
+        end
+        COUNTING_UP: begin
+          if (out == '1) begin
+            state <= COUNTING_DOWN;
+            out <= out - 1;
+          end
+          else out <= out + 1;
+        end
+        default: 
+        state <= COUNTING_UP;
+      endcase
+    end
+  end
+end
 
 endmodule
